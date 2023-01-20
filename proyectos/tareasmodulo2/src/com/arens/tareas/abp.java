7cf0a7945a5e7439b69b8e6c034a1f9700ed8090
arensrojas <arens.rojas@gmail.com>
2023-01-20 20:12:05 -0300

    segundo commit

---
+++ proyectos/tareasmodulo2/src/com/arens/tareas/abp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.arens.tareas;
+
+/**
+ * @author Usuario
+ *
+ */
+public class abp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
